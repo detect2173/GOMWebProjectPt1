a39b0d9ca800db77ccd78c2b9407f7ae26aac274
John <jfberry67@gmail.com>
2025-08-20 07:34:17 -0400

    routes(free-guide): add /free-guide/ routes; 301 redirect from /lead-magnet/

--- sitecore/urls.py
+++ sitecore/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('pricing/', views.pricing, name='pricing'),
     path('book/', views.book, name='book'),
-    path('lead-magnet/', views.lead_magnet, name='lead_magnet'),
-    path('lead-magnet/thanks/', views.lead_thanks, name='lead_thanks'),
+
+    # New canonical routes for the Free Guide
+    path('free-guide/', views.free_guide, name='free_guide'),
+    path('free-guide/thanks/', views.free_guide_thanks, name='free_guide_thanks'),
+
+    # Backward-compatible redirects from old slugs
+    path('lead-magnet/', RedirectView.as_view(pattern_name='free_guide', permanent=True)),
+    path('lead-magnet/thanks/', RedirectView.as_view(pattern_name='free_guide_thanks', permanent=True)),
+
     path('terms/', views.terms, name='terms'),
     path('privacy/', views.privacy, name='privacy'),
     path('start/', views.start, name='start'),
